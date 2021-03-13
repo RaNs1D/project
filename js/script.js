20f1072fc3122473d2911ab214a6c872f1fe5aa2
Rans1D <ransidone@gmail.com>
2021-03-13 14:15:59 +0300

    functions

--- js/script.js
+++ js/script.js
@@ -1,24 +1,213 @@
 "use strict";
 
-console.log(4 + "5");
+let numberOfFilms;
 
-let incr = 10,
-    decr = 10;
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// ++incr;
-// --decr;
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
 
-console.log(++incr);
-console.log(--decr);
+start();
 
-console.log(5%2);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    geners: [],
+    privat: false,
+};
 
-console.log(2*4 === 8);
 
-&&
-||
+function rememberMyFilms() {
+    
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов', ''),
+            b = prompt('На сколько оцените его?', '');
 
-const  isChecked = true,
-       isClose = true;
+        if (a != null &&  b != null  && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movie[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        } 
+    }
+}
 
-console.log(isChecked && isClose);
+rememberMyFilms();
+
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотренно довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киномер");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.geners[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+writeYourGenres();
+
+
+
+
+
+
+
+
+
+// const str = "teSt";
+// console.log(str.toUpperCase());
+// console.log(str.toLocaleLowerCase());
+// console.log(str);
+
+// const fruit = "Some fruit";
+// console.log(fruit.indexOf("fruit"));
+
+// const logg = "Hello world!";
+
+// console.log(logg.slice(6, 9));
+// console.log(logg.substring(6, 9));
+
+// console.log(logg.substr(6, 4));
+
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = "12.2px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+
+
+
+
+
+
+
+// let num = 20;
+// function showFirstMessage(text) {
+//     console.log(text);
+//     num = 10;
+
+// }
+
+// showFirstMessage("Hello World!");
+// console.log(num);
+
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+//     console.log("Hello");
+// };
+
+// logger();
+
+
+
+
+
+
+
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6){
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+// if (1) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if(num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 50:
+//         console.log('Верно');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
+
